3e8dfea33ae43661cc60d4ae98ff977983e96766
Yura <yury.kirytkin@yandex.ru>
2024-10-25 00:04:53 +0300

    Removed unnecessary from renderingChatButton.js

--- __tests__/renderingChatButton.js
+++ __tests__/renderingChatButton.js
@@ -1,39 +1,28 @@
 import { test, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import ChatBot from '@hexlet/chatbot-v2';
 import steps from '../__fixtures__/exampleSteps.jsx'; // мои кастомные шаги
-import { act } from 'react';
 
 window.HTMLElement.prototype.scrollIntoView = function () {}; // mock функции current.scrollIntoView , так как она отсутствует в jsdom
 
+const startChatButtonName = 'Открыть Чат';
 const defaultStyle = 'position: absolute; bottom: 20px; right: 20px;';
 
 beforeEach(async () => {
   await render(ChatBot(steps));
 });
 
-const getChatButton = async () => {
-  return screen.getByRole('button', { name: 'Открыть Чат' });
+const getStartButton = async () => {
+  return screen.getByRole('button', { name: startChatButtonName });
 };
 
 test('button visible', async () => {
-  const chatButton = await getChatButton();
-  expect(chatButton).toBeVisible();
+  const startButton = await getStartButton();
+  expect(startButton).toBeVisible();
 });
 
 test('correct button css style', async () => {
-  const chatButton = await getChatButton();
-  expect(chatButton).toHaveStyle(defaultStyle);
+  const startButton = await getStartButton();
+  expect(startButton).toHaveStyle(defaultStyle);
 });
-/*
-test('background faded after click', async () => {
-  const user = userEvent.setup();
-  const chatButton = await getChatButton();
-  await act(async () => user.click(chatButton));
-  screen.debug();
-  screen.getByLabelText
-  // expect(chatButton).toHaveStyle(defaultStyle);
-  });
-*/
