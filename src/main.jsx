8d65f99c4acac2e21bddea07c0746c3cfab529de
Yura <yury.kirytkin@yandex.ru>
2024-11-19 23:40:55 +0300

    added webpack render of imported chat module

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import Widget from '@hexlet/chatbot-v2';
 import steps from '../__fixtures__/exampleSteps.jsx'; // мои кастомные шаги
 import '@hexlet/chatbot-v2/styles';
 
+import App from './App.jsx';
+import React from 'react';
+
+// рендер пустой страницы с виджетом
+/*
 const container = document.getElementById('root');
 ReactDOM.createRoot(container)
   .render(Widget(steps));
+*/
+
+// рендер виджета как импортированного модуля
+const container = document.getElementById('root');
+ReactDOM.createRoot(container)
+  .render(<App/>);
