dde746eb6f03c7222432ef795a3d8cc2ad62b52f
Yura <yury.kirytkin@yandex.ru>
2024-11-20 23:53:21 +0300

    Wrapped in descrive main app tests

--- __tests__/appIntegration.jsx
+++ __tests__/appIntegration.jsx
@@ -1,4 +1,4 @@
-import { test, expect, beforeEach } from 'vitest';
+import { test, expect, beforeEach, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
@@ -29,38 +29,41 @@ const getButtonByName = (buttonName) => {
   return screen.getByRole('button', { name: buttonName });
 };
 
-test('All element of main App are enable', async () => { 
-  // проверка наличия всех полей ввода, кроме пароля
-  mainApp.allTextboxNames.forEach((texboxName) => {
-    const textbox = screen.getAllByRole('textbox', { name: texboxName });
-    expect(textbox).toBeVisible;
-    expect(textbox).toBeEnabled;
-  });
-
-  // проверка наличия поля ввода пароля
-  const passwordTextbox = screen.getByLabelText(mainApp.passwordTextboxName);
-  expect(passwordTextbox).toBeVisible;
-  expect(passwordTextbox).toBeEnabled;
+describe('Main app tests', () => {
   
-  // проверка наличия выпадающего списка стран
-  const countryCombobox = screen.getByRole('combobox', { name: mainApp.countryComboboxName });
-  expect(countryCombobox).toBeVisible;
-  expect(countryCombobox).toBeEnabled;
+  test('All element of main App are enable', async () => {
+    // проверка наличия всех полей ввода, кроме пароля
+    mainApp.allTextboxNames.forEach((texboxName) => {
+      const textbox = screen.getAllByRole('textbox', { name: texboxName });
+      expect(textbox).toBeVisible;
+      expect(textbox).toBeEnabled;
+    });
 
-  // проверка корректности всех опций из списка стран
-  mainApp.countryComboboxOptions.forEach((optionName) => {
-    const option = screen.getAllByRole('option', { name: optionName });
-    expect(option).toBeVisible;
-    expect(option).toBeEnabled;
+    // проверка наличия поля ввода пароля
+    const passwordTextbox = screen.getByLabelText(mainApp.passwordTextboxName);
+    expect(passwordTextbox).toBeVisible;
+    expect(passwordTextbox).toBeEnabled;
+
+    // проверка наличия выпадающего списка стран
+    const countryCombobox = screen.getByRole('combobox', { name: mainApp.countryComboboxName });
+    expect(countryCombobox).toBeVisible;
+    expect(countryCombobox).toBeEnabled;
+
+    // проверка корректности всех опций из списка стран
+    mainApp.countryComboboxOptions.forEach((optionName) => {
+      const option = screen.getAllByRole('option', { name: optionName });
+      expect(option).toBeVisible;
+      expect(option).toBeEnabled;
+    });
+
+    // проверка наличия чекбокса принятия правил
+    const rulesCheckbox = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
+    expect(rulesCheckbox).toBeVisible;
+    expect(rulesCheckbox).toBeEnabled;
+
+    // проверка наличия кнопки регистрации
+    const registerButton = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
+    expect(registerButton).toBeVisible;
+    expect(registerButton).toBeEnabled;
   });
-
-  // проверка наличия чекбокса принятия правил
-  const rulesCheckbox = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
-  expect(rulesCheckbox).toBeVisible;
-  expect(rulesCheckbox).toBeEnabled;
-
-  // проверка наличия кнопки регистрации
-  const registerButton = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
-  expect(registerButton).toBeVisible;
-  expect(registerButton).toBeEnabled;
 });
