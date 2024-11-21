1bd61ff6e7b939b46dde9a136cf0f495246a92e2
Yura <yury.kirytkin@yandex.ru>
2024-11-22 01:06:38 +0300

    Refactoring old tests and add new "main app" functionality tests

--- __tests__/appIntegration.jsx
+++ __tests__/appIntegration.jsx
@@ -10,60 +10,127 @@ window.HTMLElement.prototype.scrollIntoView = function () {}; // mock функц
 const startChatButtonName = 'Открыть Чат';
 
 const mainApp = {
-  allTextboxNames: ['Email', 'Адрес', 'Город'],
-  passwordTextboxName: 'Пароль',
-  countryComboboxName: 'Страна',
-  countryComboboxOptions: ['Выберите', 'Аргентина', 'Россия', 'Китай'],
-  rulesCheckboxName: 'Принять правила',
-  registerButtonName: 'Зарегистрироваться',
+  emailTextbox: {
+    type: 'textbox',
+    name: 'Email',
+    placeholder: 'Email',
+  },
+  passwordTextbox: {
+    type: 'passwordTextbox',
+    name: 'Пароль',
+    placeholder: 'Пароль',
+  },
+  addressTextbox: {
+    type: 'textbox',
+    name: 'Адрес',
+    placeholder: 'Невский проспект, 12',
+  },
+  cityTextbox: {
+    type: 'textbox',
+    name: 'Город',
+    placeholder: '',
+  },
+  countryCombobox: {
+    type: 'combobox',
+    name: 'Страна',
+    options: ['Выберите', 'Аргентина', 'Россия', 'Китай'],
+    defaultOption: 'Выберите',
+  },
+  rulesCheckbox: {
+    type: 'checkbox',
+    name: 'Принять правила',
+    state: false,
+  },
+  registerButton: { type: 'button', name: 'Зарегистрироваться' },
 };
+
+const wrongEmails = ['test', 'test@', '@test', '@', '@@', 'test@@test'];
 
 beforeEach(async () => {
   await render(<App />);
-  const user = userEvent.setup();
-  const startButton = await getButtonByName(startChatButtonName);
-  await act(async () => user.click(startButton));
 });
 
 const getButtonByName = (buttonName) => {
   return screen.getByRole('button', { name: buttonName });
 };
 
-describe('Main app tests', () => {
-  
-  test('All element of main App are enable', async () => {
-    // проверка наличия всех полей ввода, кроме пароля
-    mainApp.allTextboxNames.forEach((texboxName) => {
-      const textbox = screen.getAllByRole('textbox', { name: texboxName });
-      expect(textbox).toBeVisible;
-      expect(textbox).toBeEnabled;
-    });
+describe('Main app rendering tests', () => {
+  // проверка отображения всех текстбоксов
+  test.each(
+    Object.values(mainApp).filter(
+      (item) => item.name && item.name !== 'Пароль' && item.type == 'textbox',
+    ), // Фильтруем текстбоксы, исключая 'Пароль'
+    // первое item.name исключает undefined
+  )('All textboxes of main App are enabled', (element) => {
+    const realElement = screen.getByRole('textbox', { name: element.name });
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
 
-    // проверка наличия поля ввода пароля
-    const passwordTextbox = screen.getByLabelText(mainApp.passwordTextboxName);
-    expect(passwordTextbox).toBeVisible;
-    expect(passwordTextbox).toBeEnabled;
+  // проверка отображения всех комбобоксов
+  test.each(
+    Object.values(mainApp).filter((item) => item.name && item.type == 'combobox'), // Фильтруем все комбобоксы
+    // первое item.name исключает undefined
+  )('All comboboxes of main App are enabled', (element) => {
+    const realElement = screen.getByRole('combobox', { name: element.name });
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
 
-    // проверка наличия выпадающего списка стран
-    const countryCombobox = screen.getByRole('combobox', { name: mainApp.countryComboboxName });
-    expect(countryCombobox).toBeVisible;
-    expect(countryCombobox).toBeEnabled;
+  // проверка отображения всех опций комбобоксов
+  test.each(
+    Object.values(mainApp)
+      .filter((item) => item.options && item.type == 'combobox') // Фильтруем объекты, где есть options и тип combobox
+      .flatMap((item) => item.options), // Извлекаем options и объединяем в один массив
+  )('All combobox options of main App are visible and enabled', (element) => {
+    const realElement = screen.getByRole('option', { name: element });
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
 
-    // проверка корректности всех опций из списка стран
-    mainApp.countryComboboxOptions.forEach((optionName) => {
-      const option = screen.getAllByRole('option', { name: optionName });
-      expect(option).toBeVisible;
-      expect(option).toBeEnabled;
-    });
+  // проверка отображения всех текстбоксов паролей
+  test.each(
+    Object.values(mainApp).filter((item) => item.name && item.type == 'passwordTextbox'), // Фильтруем все комбобоксы
+    // первое item.name исключает undefined
+  )('All password textboxes of main App are enabled', (element) => {
+    const realElement = screen.getByLabelText(element.name);
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
 
-    // проверка наличия чекбокса принятия правил
-    const rulesCheckbox = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
-    expect(rulesCheckbox).toBeVisible;
-    expect(rulesCheckbox).toBeEnabled;
+  // проверка отображения всех чекбоксов
+  test.each(
+    Object.values(mainApp).filter((item) => item.name && item.type == 'checkbox'), // Фильтруем все комбобоксы
+    // первое item.name исключает undefined
+  )('All checkboxes of main App are enabled', (element) => {
+    const realElement = screen.getByRole('checkbox', { name: element.name });
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
 
-    // проверка наличия кнопки регистрации
-    const registerButton = screen.getByRole('checkbox', { name: mainApp.rulesCheckboxName });
-    expect(registerButton).toBeVisible;
-    expect(registerButton).toBeEnabled;
+  // проверка отображения всех кнопок
+  test.each(
+    Object.values(mainApp).filter((item) => item.name && item.type == 'button'), // Фильтруем все комбобоксы
+    // первое item.name исключает undefined
+  )('All buttons of main App are enabled', (element) => {
+    const realElement = getButtonByName(element.name);
+    expect(realElement).toBeEnabled;
+    expect(realElement).toBeVisible;
+  });
+});
+
+describe('Main app functional tests', async () => {
+  //проверка того, что неправильные почты не сработают и не покажут результирующую таблицу
+  test.each(wrongEmails)('Email textbox limitations', async (wrongEmail) => {
+    const user = userEvent.setup();
+    const emailTextbox = screen.getByRole('textbox', { name: mainApp.emailTextbox.name });
+    const registerButton = getButtonByName(mainApp.registerButton.name);
+    await act(async () => user.clear(emailTextbox));
+    await act(async () => user.type(emailTextbox, wrongEmail));
+    await act(async () => user.click(registerButton));
+    expect(emailTextbox).toBeEnabled();
+    expect(emailTextbox).toBeVisible();
+    expect(registerButton).toBeEnabled();
+    expect(registerButton).toBeVisible();
   });
 });
