634b94c88324184626ee9a3f476e099f0a3adbcc
Yura <yury.kirytkin@yandex.ru>
2024-11-28 22:15:59 +0300

    Remade test files to kebab-case names

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import ReactDOM from 'react-dom/client';
-// import Widget from '@hexlet/chatbot-v2';
-// import steps from '@hexlet/chatbot-v2/example-steps'; // стандартные шаги
-// import steps from '../__fixtures__/exampleSteps.jsx'; // мои кастомные шаги
 import '@hexlet/chatbot-v2/styles';
-
-import App from './App.jsx';
-import React from 'react';
-
+/*
 // рендер пустой страницы с виджетом
-
+import Widget from '@hexlet/chatbot-v2';
+// import steps from '@hexlet/chatbot-v2/example-steps'; // стандартные шаги
+ import steps from '../__fixtures__/exampleSteps.jsx'; // мои кастомные шаги
 const container = document.getElementById('root');
 ReactDOM.createRoot(container)
   .render(Widget(steps));
-
-/*
+*/
 // рендер виджета как импортированного модуля
+import App from './App.jsx';
+import React from 'react';
 const container = document.getElementById('root');
 ReactDOM.createRoot(container)
   .render(<App/>);
-*/
