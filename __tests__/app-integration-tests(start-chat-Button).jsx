7a164624519909a89e80643830cc76bca3bec34e
Yura <yury.kirytkin@yandex.ru>
2024-12-03 23:48:01 +0300

    Remade app with plugun integration tests to PO classes

--- __tests__/app-integration-tests(start-chat-Button).jsx
+++ __tests__/app-integration-tests(start-chat-Button).jsx
@@ -2,6 +2,7 @@ import { test, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../src/App.jsx';
+import { ChatPluginStartButton } from './src/app-integration-tests/chat-plugin-tests.js';
 import { chatFixtures } from '../__fixtures__/chat-fixtures.js';
 
 window.HTMLElement.prototype.scrollIntoView = function () {}; // mock функции current.scrollIntoView , так как она отсутствует в jsdom
@@ -10,12 +11,8 @@ beforeEach(async () => {
   await render(<App />);
 });
 
-const getButtonByName = (buttonName) => {
-  return screen.getByRole('button', { name: buttonName });
-};
-
 test('button is correct', async () => {
-  const startButton = await getButtonByName(chatFixtures.startChatButtonName);
-  expect(startButton).toBeVisible();
-  expect(startButton).toHaveStyle(chatFixtures.defaultChatStyle);
+  const chatPlugin = new ChatPluginStartButton(screen);
+  expect(chatPlugin.startChatButton).toBeVisible();
+  expect(chatPlugin.startChatButton).toHaveStyle(chatFixtures.defaultChatStyle);
 });
