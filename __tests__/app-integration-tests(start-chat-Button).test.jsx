153eda5457e38a39f9b891ddc1d75edccc82a52e
Yura <yury.kirytkin@yandex.ru>
2024-12-04 23:57:36 +0300

    Fixed a bug with incorrect default import

--- __tests__/app-integration-tests(start-chat-Button).test.jsx
+++ __tests__/app-integration-tests(start-chat-Button).test.jsx
@@ -2,7 +2,7 @@ import { test, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../src/App.jsx';
-import { ChatPluginStartButton } from './src/app-integration-tests/page-object-classes/ChatPluginStartButtonPO.js';
+import ChatPluginStartButton from './src/app-integration-tests/page-object-classes/ChatPluginStartButtonPO.js';
 import { chatFixtures } from '../__fixtures__/chat-fixtures.js';
 
 window.HTMLElement.prototype.scrollIntoView = function () {}; // mock функции current.scrollIntoView , так как она отсутствует в jsdom
